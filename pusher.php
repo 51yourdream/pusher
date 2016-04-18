44ab5e7e750c1e5f4e390e5fdd4ff3407d4e8334
Brucelee123 <lpjustdoit@163.com>
2016-04-18 14:28:27 +0000

    tui song test1

--- pusher.php
+++ pusher.php
@@ -16,4 +16,4 @@ $options = [
 $pusher = new Pusher( $app_key, $app_secret, $app_id );
 
 $data['message'] = 'hello world';
-$pusher->trigger('test_channel', 'my_event', $data);
+$pusher->trigger('test_channel1', 'my_event', $data);
